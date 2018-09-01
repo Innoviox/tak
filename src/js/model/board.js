7f299a1f2df27ebde1abc0f13c4c902a3987561d
Innoviox <sarras305@gmail.com>
2018-08-31 21:17:00 -0400

    Untested move logic

--- src/js/model/board.js
+++ src/js/model/board.js
@@ -4,10 +4,22 @@ var LEFT = "<",
     DOWN = "-";
 
 class Position {
-  constructor(x, y) {
-    this.x = x;
-    this.y = y;
-  }
+    constructor(x, y) {
+        this.x = x;
+        this.y = y;
+    },
+
+    next(dir) {
+        if (dir == LEFT) {
+            return Position(this.x - 1, this.y);
+        } else if (dir == RIGHT) {
+            return Position(this.x + 1, this.y);
+        } else if (dir == UP) {
+            return Position(this.x, this.y - 1);
+        } else {
+            return Position(this.x, this.y + 1);
+        }
+    }
 }
 
 class Move {
@@ -18,6 +30,7 @@ class Move {
         this.moves = moves;
         this.dir = dir;
     }
+
 }
 
 class Tile {
@@ -121,21 +134,39 @@ var Board = {
     Execute a full move
     */
     move: function(move) {
-      var old_pos = move.pos;
-      var new_pos = move.pos.next(move.dir);
-      if (move.moves.length > 0) {
-        var first = true;
-        for (idx in move.moves) {
-          this._move(old_pos, new_pos, move.moves[idx], first);
-          first = false;
+        var old_pos = move.pos;
+        var new_pos = move.pos.next(move.dir);
+        if (move.moves.length > 0) {
+            var first = true;
+            for (idx in move.moves) {
+                this._move(old_pos, new_pos, move.moves[idx], first);
+                first = false;
+            }
         }
-      }
     },
 
     /*
     One step of a move
     */
     _move: function(old_pos, new_pos, n, first) {
+        var old_sq = this.board[old_pos.x][old_pos.y];
+        var new_sq = this.board[new_pos.x][new_pos.y];
 
+        var tiles = old_sq.tiles.slice(-n);
+        var n_stone = new_pos.tiles.slice(-1)[0].stone;
+        if (new_pos.tiles.length == 0 || (n_stone == FLAT)) {
+            old_sq.tiles = old_sq.tiles(0, old_sq.tiles.length - n);
+            for (idx in tiles) {
+              new_sq.add(tiles[idx]);
+            }
+        } else if (tiles.slice(-1)[0].stone == CAP && n_stone == STAND) {
+            old_sq.tiles = old_sq.tiles(0, old_sq.tiles.length - n);
+            new_sq.tiles.slice(-1)[0].stone = FLAT;
+            for (idx in tiles) {
+              new_sq.add(tiles[idx]);
+            }
+        } else {
+          //TODO Throw error?
+        }
     }
 }
