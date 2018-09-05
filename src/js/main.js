f5c21fde8504f30a6058569690010f3291b98e00
Innoviox <sarras305@gmail.com>
2018-09-05 17:53:02 -0400

    Deleted comments

--- src/js/main.js
+++ src/js/main.js
@@ -141,23 +141,12 @@ function toString(v) {
 }
 
 function onDocumentMouseDown(event) {
-    // the following line would stop any other event handler from firing
-    // (such as the mouse's TrackballControls)
-    // event.preventDefault();
-
-    console.log("Click.");
-
-    // update the mouse variable
     mouse.x = (event.clientX / window.innerWidth) * 2 - 1;
     mouse.y = -(event.clientY / window.innerHeight) * 2 + 1;
 
-    // find intersections
-    // create a Ray with origin at the mouse position
-    //   and direction into the scene (camera direction)
     var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
     vector.unproject(camera);
     var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
-    // create an array containing all objects in the scene with which the ray intersects
     for (tile of Board.tiles)
         scene.remove(tile);
 
@@ -166,13 +155,12 @@ function onDocumentMouseDown(event) {
     for (tile of Board.tiles)
         scene.add(tile);
 
-    // if there is one (or more) intersections
     if (intersects.length > 0) {
-        console.log(intersects);
-        console.log("Hit @ " + toString(intersects[0].point));
-        // change the color of the closest face.
-        intersects[0].face.color.setRGB(0.8 * Math.random() + 0.2, 0, 0);
-        intersects[0].object.geometry.colorsNeedUpdate = true;
+        for (obj of intersects) {
+            if (obj.object.name == "square") {
+                console.log(obj);
+            }
+        }
     }
 }
 
@@ -199,16 +187,10 @@ function animate() {
 }
 
 function update() {
-    // find intersections
-
-    // create a Ray with origin at the mouse position
-    //   and direction into the scene (camera direction)
     var vector = new THREE.Vector3(mouse.x, mouse.y, 1);
-    // projector.unprojectVector( vector, camera );
     vector.unproject(camera);
     var ray = new THREE.Raycaster(camera.position, vector.sub(camera.position).normalize());
 
-    // create an array containing all objects in the scene with which the ray intersects
     for (tile of Board.tiles)
         scene.remove(tile);
 
@@ -217,32 +199,20 @@ function update() {
     for (tile of Board.tiles)
         scene.add(tile);
 
-    // INTERSECTED = the object in the scene currently closest to the camera
-    //		and intersected by the Ray projected from the mouse position
-
-    // if there is one (or more) intersections
     if (intersects.length > 0) {
-        // if the closest object intersected is not the currently stored intersection object
         if (intersects[0].object != INTERSECTED) {
-            // restore previous intersection object (if it exists) to its original color
             if (INTERSECTED)
                 INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
             if (intersects[0].object.name == "square") {
-                // store reference to closest object as current intersection object
                 INTERSECTED = intersects[0].object;
-                // store color of closest object (for later restoration)
                 INTERSECTED.currentHex = INTERSECTED.material.color.getHex();
-                // set a new color for closest object
                 INTERSECTED.material.color.setHex(0xffff00);
             }
         }
-    } else { // there are no intersections
-        // restore previous intersection object (if it exists) to its original color
+    } else {
         if (INTERSECTED)
             INTERSECTED.material.color.setHex(INTERSECTED.currentHex);
 
-        // remove previous intersection object reference
-        //     by setting current intersection object to "nothing"
         INTERSECTED = null;
     }
 }
