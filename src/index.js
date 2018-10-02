86cbe0da340570a57daf902bfc15f45a43bf6a48
Simon Chervenak <sarras305@gmail.com>
2018-10-02 17:52:29 -0400

    Working with airtable

--- src/index.js
+++ src/index.js
@@ -3,6 +3,26 @@ var app = express();
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
+var Airtable = require('airtable');
+Airtable.configure({
+    endpointUrl: 'https://api.airtable.com',
+    apiKey: ''
+});
+var base = Airtable.base('appvViVoTQrAVwGwR');
+
+base('Table 1').select({
+    view: "Grid view"
+}).eachPage(function page(records, fetchNextPage) {
+    records.forEach(function(record) {
+        console.log('Retrieved', record.get('Username'));
+    });
+
+    fetchNextPage();
+
+}, function done(err) {
+    if (err) { console.error(err); return; }
+});
+
 // Serve scripts, css, and resources
 app.use(express.static('public'));
 
@@ -16,5 +36,5 @@ io.on('connection', function(socket){
 });
 
 http.listen(3001, function(){
-  console.log('listening on 0.0.0.0:3000');
+  console.log('listening on 0.0.0.0:3001');
 });
--- src/public/js/server/socket.js
+++ src/public/js/server/socket.js
@@ -1,6 +1,1 @@
-var express = require('express');
-var app = express();
-var http = require('http').Server(app);
-var io = require('socket.io')(http);
-
 var socket = io();
